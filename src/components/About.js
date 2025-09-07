236a331fddeb70220f06501ce44203e533aef232
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2025-09-07 23:43:18 +0600

    about section completed

--- src/components/About.js
+++ src/components/About.js
@@ -15,11 +15,13 @@ const About = () => {
     <section className='section' id='about' ref={ref} >
       <div className="container mx-auto">
         <div className=" flex flex-col gap-y-10 mt-[50px]  lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen  ">
-          <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.3}}  className=" flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top "></motion.div>
+          <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.3}}  className=" flex-1 bg-about bg-contain bg-no-repeat min-h-[300px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[620px] mix-blend-lighten bg-top "></motion.div>
           <motion.div variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.3}}  className=" flex-1 ">
             <h2 className=" h2 text-accent ">About me</h2>
-            <h3 className=' h3 mb-4 ' >I'm a Freelance Front-end Developer with over 3 years of experience.</h3>
-            <p className=" mb-6 ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam dolorem impedit vero odit perferendis corrupti similique modi placeat laboriosam numquam!</p>
+            <h3 className=' h3 mb-2 ' >I'm a Full Stack Developer with over 3 years of experience.</h3>
+            <p className=" mb-6 ">
+             I specialize in React, Next.js, and React Native, and also work with Node.js, Express, and databases like MongoDB and MySQL to deliver scalable solutions. With a strong focus on clean code, performance, and user-friendly design.
+            </p>
             <div className="flex gap-x-6 lg:gap-x-10 mb-12  ">
               <div className="">
                 <div className=" text-[40px] font-tertiary text-gradient mb-2  ">
@@ -35,17 +37,17 @@ const About = () => {
                 <div className=" text-[40px] font-tertiary text-gradient mb-2  ">
                   {
                     inView ?
-                    <CountUp start={0} end={16} duration={3} /> : null }k+
+                    <CountUp start={0} end={46} duration={3} /> : null }+
                 </div>
                 <div className=" font-primary text-sm tracking-[2px]  ">
                   Projects <br />
                   Completed</div>
-              </div>
+              </div> 
               <div className="">
                 <div className=" text-[40px] font-tertiary text-gradient mb-2  ">
                   {
                     inView ?
-                    <CountUp start={0} end={12} duration={3} /> : null }k+
+                    <CountUp start={0} end={12} duration={3} /> : null }+
                 </div>
                 <div className=" font-primary text-sm tracking-[2px]  ">
                   Satisfied<br />
